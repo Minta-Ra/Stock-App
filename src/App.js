9210774e2b4ca8ac4a50b9259233e9b7b6db83bc
Minta-Ra <r.razauskaite@gmail.com>
2020-06-05 14:30:02 +0100

    app started

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1>Hi, This is stock app and I am on Ramintas computer!</h1>
+    <h1>Good Luck and Never Give Up!!</h1>
+    </div>
+  );
+}
+
+export default App;
