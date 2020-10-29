a9ba4c1ab4374d47cc0db65d759c90b6b22c4552
Piotr <piotr@Piotrs-MacBook-Pro.local>
2020-10-29 18:30:52 +0000

    added changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import Blah from './blah'
 
 function App() {
   return (
     <div>
+    
     <h1>Hi, This is stock app and I am on Ramintas computer!</h1>
     <h1>Good Luck and Never Give Up!!</h1>
+    <h1>0050</h1>
     </div>
   );
 }
---
+++ src/blah.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+class Blah extends React.Component {
+  constructor(){
+
+    render(){
+      return(
+        <p>hi from blah</p>
+      )
+    }
+  }
+}
+
+
+export default Blah
